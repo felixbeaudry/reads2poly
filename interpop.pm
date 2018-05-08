67efa6e296772a5c552cce451ec45f057c8beb58
Felix Beaudry <felix.beaudry@gmail.com>
2018-05-08 19:34:41 -0400

    Create interpop.pm

---
+++ interpop.pm
@@ -0,0 +1,18 @@
+sub popIDs
+{my($filename) = @_;
+if $line ~ "pop1" {
+    split{$line};
+    for($x=0; $x>len(line); x++){
+      my $pop1[$x] = $split_line
+    }
+}
+elsif $line ~ "pop2" {--ditto--}
+
+
+sub interPopStats
+{my($filename) = @_;
+
+#inloop
+#in file
+
+#outloop
