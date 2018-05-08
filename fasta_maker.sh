82fa6d08d45f353f9d56dd168fbf8cdaadbf0f2b
Felix Beaudry <felixbeaudry@esc2042-imac-hollister-wright.eeb.utoronto.ca>
2018-05-08 16:59:43 -0400

    fasta_maker udpate

--- fasta_maker.sh
+++ fasta_maker.sh
@@ -2,24 +2,25 @@
 #script to make fastas of all sequences
 #Felix Beaudry 8 May 2018
 
-#run with: bash fasta_maker.sh inds.list loci.list outDir
+#run with: bash /ohta/felix.beaudry/scripts/reads2poly/fasta_maker.sh inds.list loci.list allFasta
 
 
 ind_list=$1
 loc_list=$2
 
 outDir=$3
-rm $outDir/*
 
 while read ind 
 do
 while read loc
 do
+rm $outDir/$loc.fasta
 python /ohta/felix.beaudry/scripts/reads2poly/fasta_cleaner.py -i ${ind}/${loc}.fasta | cat >> $outDir/${loc}.fasta
 #perl /ohta/felix.beaudry/scripts/reads2poly/codoner.pl
 #align to outgroup, in frame, with PRANK
 #send in to polymorphurama
 done < $loc_list
 done < $ind_list
+
 
 
