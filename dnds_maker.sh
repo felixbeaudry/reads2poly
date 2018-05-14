b8510553433a24e1d5c694c2046ba04ef93b3277
Felix Beaudry <felixbeaudry@esc2042-imac-hollister-wright.eeb.utoronto.ca>
2018-05-14 12:17:58 -0400

    maker update

---
+++ dnds_maker.sh
@@ -0,0 +1,8 @@
+##dnds maker
+#find outgroup sequence using BLAST
+#add frame to outgroup sequence
+#perl /ohta/felix.beaudry/scripts/reads2poly/codoner.pl $outDir/${loc}.fasta
+#align to outgroup, in frame - using codon model -, with PRANK
+#/ohta/felix.beaudry/scripts/prank/bin/prank -d=.fasta -o=prank/.fasta -codon -F
+#send into PAML
+
--- fasta_maker.sh
+++ fasta_maker.sh
@@ -18,11 +18,11 @@ done < $loc_list
 while read ind 
 do
 echo "Adding sequences from $ind"
+perl /ohta/felix.beaudry/scripts/reads2poly/vcf2fasta_uni.pl  -v ${ind}.uni.vcf -o ${ind} -l ${ind}.logfile -a T 2>${ind}/errors.txt
+
 while read loc
 do
-echo "${loc}"
-perl /ohta/felix.beaudry/scripts/reads2poly/vcf2fasta_uni.pl  -v ${ind}.uni.vcf -o ${ind} -l ${ind}.logfile -a T 
-
+echo -e "\t${loc}"
 python /ohta/felix.beaudry/scripts/reads2poly/fasta_cleaner.py -i ${ind}/${loc}.fasta 2>$outDir/errors.txt | cat >> $outDir/${loc}.fasta
 
 done < $loc_list
@@ -30,13 +30,5 @@ done < $ind_list
 
 #send in to polymorphurama
 perl /ohta/felix.beaudry/scripts/reads2poly/polymorphurama_interpop.pl fasta $outDir
-
-#find outgroup sequence using BLAST
-#add frame to outgroup sequence
-#perl /ohta/felix.beaudry/scripts/reads2poly/codoner.pl $outDir/${loc}.fasta
-#align to outgroup, in frame - using codon model -, with PRANK
-#/ohta/felix.beaudry/scripts/prank/bin/prank -d=.fasta -o=prank/.fasta -codon -F
-#send into PAML
-
 
 
