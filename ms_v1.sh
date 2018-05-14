086fa80e336ce2897ca035656a423ac23ed5cb01
Felix Beaudry <felixbeaudry@esc2042-imac-hollister-wright.eeb.utoronto.ca>
2018-05-14 15:59:44 -0400

    maker update

--- ms_v1.sh
+++ ms_v1.sh
@@ -1,3 +1,10 @@
 ms nsam nreps -t θ
 #nsam is the number of copies of the locus in each sample, and 
 #nreps is the number of independent samples to generate.
+		#what is nsam and nreps
+
+ms nsam nreps -t θ -I npop n1 n2 ...[4N0m]
+
+/ohta/felix.beaudry/scripts/msdir/ms 23 10 -t 0.006 -s 7
+
+		#how do I make my demographic model
