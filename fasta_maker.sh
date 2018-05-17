54ea16b3e952401ffab29941b33b237ace1a3943
Felix Beaudry <felixbeaudry@esc2042-imac-hollister-wright.eeb.utoronto.ca>
2018-05-17 15:05:28 -0400

    fasta update

--- fasta_maker.sh
+++ fasta_maker.sh
@@ -19,19 +19,20 @@ done < $loc_list
 while read ind 
 do
 echo "Adding sequences"
+if [ -d "${ind}" ]; then
+echo "${ind} fastas exist"
+else	
 perl /ohta/felix.beaudry/scripts/reads2poly/vcf2fasta_uni.pl  -v ${ind}.uni.vcf -o ${ind} -l ${ind}.logfile -a T 2>${ind}/errors.txt
-
+fi
 while read loc
 do
 echo -e "${ind}\t${loc}"
 python /ohta/felix.beaudry/scripts/reads2poly/fasta_cleaner.py -i ${ind}/${loc}.fasta 2>$outDir/errors.txt | cat >> $outDir/${loc}.fasta
-#python /ohta/felix.beaudry/scripts/reads2poly/fasta_cleaner.py -i 3_Index-10.TM4/Locus_9983_Transcript_2_Length_951_genewise_newlength_951_CDSlength_645.fasta | cat >> allFasta/Locus_9983_Transcript_2_Length_951_genewise_newlength_951_CDSlength_645.fasta 
-
 
 done < $loc_list
 done < $ind_list
 
 #send in to polymorphurama
-perl /ohta/felix.beaudry/scripts/reads2poly/polymorphurama_interpop.pl fasta $outDir
+#perl /ohta/felix.beaudry/scripts/reads2poly/polymorphurama_interpop.pl fasta $outDir
 
 
