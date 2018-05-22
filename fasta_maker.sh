dec27e4f2c5a5fa11ccef3a9d97bcc1123f55488
Felix Beaudry <felixbeaudry@esc2042-imac-hollister-wright.eeb.utoronto.ca>
2018-05-22 10:25:05 -0400

    fasta_cleaner update

--- fasta_maker.sh
+++ fasta_maker.sh
@@ -2,12 +2,13 @@
 #script to make fastas of all sequences
 #Felix Beaudry 8 May 2018
 
-#run with: bash /ohta/felix.beaudry/scripts/reads2poly/fasta_maker.sh inds.list loci.list allFasta cutoff
+#run with: bash /ohta/felix.beaudry/scripts/reads2poly/fasta_maker.sh inds.list loci.list allFasta cutoff T/F
 
 ind_list=$1
 loc_list=$2
 outDir=$3
 cutoff=$4
+makefasta=$5
 
 mkdir ${outDir}
 awk 'split($1,a,"."){print a[1]}' ${loc_list} >${outDir}/${loc_list}
@@ -20,7 +21,7 @@ done < ${outDir}/${loc_list}
 while read ind 
 do
 echo "Adding sequences"
-if [ -d "${ind}" ]; then
+if $makefasta = "F"; then
 echo -e "${ind}\tfastas exist"
 else
 perl /ohta/felix.beaudry/scripts/reads2poly/vcf2fasta_uni.pl  -v ${ind}.uni.vcf -o ${ind} -l ${ind}.logfile -a T -m ${cutoff} 2>${ind}_vcferrors.txt
