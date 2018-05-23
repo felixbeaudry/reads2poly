6b6f40abc7fe9312290c4e451faa69ae8a8b338c
Felix Beaudry <felixbeaudry@esc2042-imac-hollister-wright.eeb.utoronto.ca>
2018-05-23 17:30:54 -0400

    dnds_maker

--- dnds_maker.sh
+++ dnds_maker.sh
@@ -2,33 +2,37 @@
 #script to get dnds from outgroup
 #Felix Beaudry 14 May 2018
 
-#run with: bash /ohta/felix.beaudry/scripts/reads2poly/dnds_maker.sh goodsex buc /ohta/felix.beaudry/assemblies/bucephalophorusTranscriptome/RB1_transcriptome_ref.fa
+#run with: bash /ohta/felix.beaudry/scripts/reads2poly/dnds_maker.sh goodsex buc /ohta/felix.beaudry/assemblies/bucephalophorusTranscriptome/RB1_transcriptome_ref.fa 
 
-#run with: bash /ohta/felix.beaudry/scripts/reads2poly/dnds_maker.sh goodsex roth /ohta/felix.beaudry/assemblies/rothTranscriptome/roth_female_ref_genewise.fa
+#run with: bash /ohta/felix.beaudry/scripts/reads2poly/dnds_maker.sh goodsex roth /ohta/felix.beaudry/assemblies/rothTranscriptome/roth_female_ref_genewise.fa goodsex.fasta.list
 
 
 outDir=$1
 outgroupName=$2
 outgroupTranscriptome=$3
+locList=$4
 
-##make database
-#/ohta/aplatts/data/apps/ncbi-blast-2.6.0+/bin/makeblastdb -in RB1_transcriptome_ref.fa -dbtype nucl
-##make match file
-#mkdir ${outgroupName}
-#echo "Blasting ${outgroupName}"
-#/ohta/aplatts/data/apps/ncbi-blast-2.6.0+/bin/blastn -task megablast -query /ohta/felix.beaudry/assemblies/hastTranscriptome/NCF1_combined_ref_CDS_noStop.fa -db ${outgroupTranscriptome} -num_threads 10 -max_target_seqs 1 -outfmt 5 -dust no -gapopen 0 -gapextend 0 >${outgroupName}/${outgroupName}_blast.txt
-
-##parse
-echo "Parsing ${outgroupName}"
-python /ohta/felix.beaudry/scripts/reads2poly/blast2fullfasta.py -i ${outgroupName}/${outgroupName}_blast.txt -o ${outgroupName}
-
-ls ${outgroupName} >${outgroupName}/loc.list
-#awk 'split($1,a,"."){print a[1]}' ${loc_list} >${outgroupName}/loc.list
-
+mkdir ${outgroupName}
 while read loc
 do
-cat ${outDir}/${loc} >>${outgroupName}/${loc}
-done < ${outgroupName}/loc.list
+##make database
+#/ohta/aplatts/data/apps/ncbi-blast-2.6.0+/bin/makeblastdb -in ${outgroupTranscriptome} -dbtype nucl
+##make match file
+mkdir ${outgroupName}/${loc}
+echo "Blasting ${loc}"
+/ohta/aplatts/data/apps/ncbi-blast-2.6.0+/bin/blastn -task megablast -query ${outDir}/${loc} -db ${outgroupTranscriptome} -num_threads 10 -max_target_seqs 1 -outfmt 5 -dust no -gapopen 0 -gapextend 0 >${outgroupName}/${loc}.blast
+
+##parse
+echo "Parsing ${loc}"
+python /ohta/felix.beaudry/scripts/reads2poly/blast2fullfasta.py -i ${outgroupName}/${loc}.blast -o ${outgroupName}/${loc}
+done < $locList
+
+#awk 'split($1,a,"."){print a[1]}' ${loc_list} >${outgroupName}/loc.list
+
+#while read loc
+#do
+#cat ${outDir}/${loc} >>${outgroupName}/${loc}
+#done < ${outgroupName}/loc.list
 
 #while read loc
 #do
