54507044ecb80e76a3310b4595b6f52273034cda
Felix Beaudry <felixbeaudry@esc2042-imac-hollister-wright.eeb.utoronto.ca>
2018-05-23 09:55:21 -0400

    blast_cleaner update

--- blast_cleaner.py
+++ blast_cleaner.py
@@ -4,32 +4,35 @@
 #Felix Beaudry 22 May 2018
 
 import fileinput, argparse
-from numpy import array, zeros
+from numpy import array, zeros, concatenate
 
 def arguments():
         parser  = argparse.ArgumentParser(description="take blast output and make into a best-hit fasta file")
         parser.add_argument("-i","--input",help="input",required=True)
+        parser.add_argument("-o","--outdir",help="output directory",required=True)
         args = parser.parse_args()
         return(args)
 
 args = arguments()
 inpath = args.input
+outpath = args.outdir
 infile = open(inpath,'r')
 
-lociArray = (['empty'],['empty'])
-rowCount = 0
+
 
 for line in infile:
 	if "<Iteration_query-def>" in line :
+		
+		#cut at ">" and "<"
 		line.rstrip()
 		early = line.split('>')
 		earlyString = str(early[1])
 		late = earlyString.split('<')
-		
-		lociArray.append(str(late[0]))
-		rowCount += 1
-		#cut at ">" and "<"
 		#store
+		
+		locus_name = str(late[0])
+		line_all = ''
+
 	if "<Hsp_hseq>" in line :
 		early = line.split('>')
 		earlyString = str(early[1])
@@ -37,17 +40,22 @@ for line in infile:
 
 		#make two celled array of name and sequence (including adding the sequence together)
 		#append the two-celled array to the overall array
+		line_clean = str(late[0])
+		line_all = line_all + line_clean
 
+	if "</Hit>" in line :
 		
-		name = str(lociArray[rowCount])
-		file_name =  "test_files/roth/"+name+".fasta"
+		file_name = outpath+locus_name+'.fasta'
+		file = open(file_name,'w') 
+		file.write(">roth\n")
+		for i in range(0, len(line_all), 60):
+			file.write(line_all[i:i+60])
+			file.write('\n') 
+		file.close() 
 
 #print as cells, with frames of sixty
 
-		file = open(file_name,'w') 
-		file.write(">roth\n")
-		file.write(late[0]) 
-		file.close() 
+
 
 
 		
--- dnds_maker.sh
+++ dnds_maker.sh
@@ -2,49 +2,31 @@
 #script to get dnds from outgroup
 #Felix Beaudry 14 May 2018
 
-#run with: bash /ohta/felix.beaudry/scripts/reads2poly/dnds_maker.sh loci.list allFasta nostop2buc.fasta
+#run with: bash /ohta/felix.beaudry/scripts/reads2poly/dnds_maker.sh auto_loci.list allFasta roth /ohta/felix.beaudry/assemblies/bucephalophorusTranscriptome/RB1_transcriptome_ref.fa
 loc_list=$1
 outDir=$2
-outgroup_assembly=$3
-outgroup_name=$4
+outgroupName=$3
+outgroupTranscriptome=$4
 
-if [#exists]; then
-echo "Query exists"
-else
 ##make database
-/ohta/aplatts/data/apps/ncbi-blast-2.6.0+/bin/makeblastdb -in RB1_transcriptome_ref.fa -dbtype nucl
+#/ohta/aplatts/data/apps/ncbi-blast-2.6.0+/bin/makeblastdb -in RB1_transcriptome_ref.fa -dbtype nucl
 ##make match file
-/ohta/aplatts/data/apps/ncbi-blast-2.6.0+/bin/blastn -task megablast -query /ohta/felix.beaudry/assemblies/hastTranscriptome/NCF1_combined_ref_CDS_noStop.fa -db /ohta/felix.beaudry/assemblies/bucephalophorusTranscriptome/RB1_transcriptome_ref.fa -num_threads 10 -max_target_seqs 1 -outfmt 5 -dust no -gapopen 0  -gapextend 0 >nostop2buc/nostop2buc.out
-fi
+mkdir ${outgroupName}
+echo "Blasting ${outgroupName}"
+/ohta/aplatts/data/apps/ncbi-blast-2.6.0+/bin/blastn -task megablast -query /ohta/felix.beaudry/assemblies/hastTranscriptome/NCF1_combined_ref_CDS_noStop.fa -db ${outgroupTranscriptome} -num_threads 10 -max_target_seqs 1 -outfmt 5 -dust no -gapopen 0 -gapextend 0 >${outgroupName}/${outgroupName}_blast.txt
 
 ##parse
-python blast_cleaner.py
-#awk '$1 ~ "<Iteration_query-def>" ||  $1 ~ "<Hsp_hseq>" {print}' nostop2buc/nostop2buc.out | sed ':a;N;$!ba;s/<Hit_def>//g' | sed ':a;N;$!ba;s/<Iteration_query-def>//g' | sed ':a;N;$!ba;s/<Hsp_qseq>//g' | sed ':a;N;$!ba;s/<Hsp_hseq>//g' | sed ':a;N;$!ba;s/<\/Hsp_qseq>//g'| sed ':a;N;$!ba;s/<\/Iteration_query-def>//g'  | sed ':a;N;$!ba;s/      //g' | sed ':a;N;$!ba;s/<\/Hsp_hseq>\n//g' | sed ':a;N;$!ba;s/  Locus/\n\n>Locus/g' >nostop2buc.fasta
+echo "Parsing ${outgroupName}"
+python blast_cleaner.py -i ${outgroupName}/${outgroupName}_blast.txt -o ${outgroupName}
 
-
-if [ -d "${outDir}/prank" ]; then
-	rm ${outDir}/prank/*
-else
-	mkdir ${outDir}/prank
-fi
-
-samtools ${outgroup_assembly} ${loc}  | cat ${outDir}/${loc} > prank/${loc}
-
+mkdir ${outgroupName}/prank
 while read loc
 do
-
-##take output from fasta_maker and add outgroup
-#samtools faidx nostop2roth.fasta ${loc} | cat $outdir/$loc >>${ind}/${ind}_${loc}.fasta
-
-##add frame to outgroup sequence
-perl /ohta/felix.beaudry/scripts/reads2poly/codoner.pl ${outDir}/${loc}.fasta >${outDir}/prank/${loc}_cod.fasta
-
-
-
+echo "Aligning ${loc}"
+perl /ohta/felix.beaudry/scripts/reads2poly/codoner.pl ${outgroupName}/${loc}.fasta >${outgroupName}/${loc}_cod.fasta
+samtools faidx ${outgroupName}/${loc}_cod.fasta ${outgroupName} | cat ${outDir}/${loc} > ${outgroupName}/${loc}.fasta
+##align to outgroup, in frame - using codon model -, with PRANK
+/ohta/felix.beaudry/scripts/prank/bin/prank -d=${outgroupName}/${loc}.fasta -o=${outgroupName}/prank/${loc} -codon -F
 done < $loc_list
 
-##align to outgroup, in frame - using codon model -, with PRANK
-/ohta/felix.beaudry/scripts/prank/bin/prank -d=${outDir}/prank/${loc}_cod.fasta -o=prank/${loc} -codon -F
-
-##PAML
 
