48210be37daf30e38c972b266a801df6a1a15681
Felix Beaudry <felixbeaudry@esc2042-imac-hollister-wright.eeb.utoronto.ca>
2018-05-24 15:05:22 -0400

    dnds_maker

--- blast2fullfasta.py
+++ blast2fullfasta.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 #python
 #blast_cleaner: script to take blast output and make into a best-hit fasta file
+#python blast2fullfasta.py -i <outgroup>.blast -o  
+
 #Felix Beaudry 22 May 2018
 
 import fileinput, argparse
@@ -34,6 +36,7 @@ hitStart= 0
 hlength = 0
 extraSpots = [1]
 locus_name = ""
+hitCount = 0
 
 for line in infile:
 	if "<Iteration_query-def>" in line :
@@ -43,78 +46,105 @@ for line in infile:
 		qlength = 0
 		hlength = 0
 		extraSpots = [1]
-		#print locus name (will later turn this into file name)
 		locus_name = blastParser(line)
+		#print locus_name
 
 	if "<Iteration_query-len>" in line :
 		#reference length
 		qlength = int(blastParser(line))
+		#print "qlength"
+		#print qlength
 
-	if "<Hsp_hit-from>" in line:
-		hitStart = int(blastParser(line))
-
-	if "<Hsp_hit-to>" in line:
-		hlength = int(blastParser(line)) - hitStart
-
+	if "<Hsp_num>" in line :
+		hitCount = int(blastParser(line))
 
 	if "<Hsp_query-from>" in line:
 		#where to start filling sequence array with hit sequence
-		start = int(blastParser(line)) - 1  
+		if hitCount == 1 :
+			start = int(blastParser(line)) - 1 
+			#print "start"
+			#print start
+		else :
+			pass 
+		
+
+	if "<Hsp_hit-from>" in line:
+		if hitCount == 1 :
+			hitStart = int(blastParser(line))
+		else :
+			pass 
+
+	if "<Hsp_hit-to>" in line:
+		if hitCount == 1 :
+			hlength = int(blastParser(line)) - hitStart
+			#print "hlength"
+			#print hlength
+		else :
+			pass 
 
 	#make array of spots where the query was extended
 	if "<Hsp_qseq>" in line:
-		#split query where it has been extended
-		lineSplit = blastParser(line).split("-")
-		#make array the right length
-		extraSpots = extraSpots * (len(lineSplit) -1)
+		if hitCount == 1 :
+			#split query where it has been extended
+			lineSplit = blastParser(line).split("-")
+			#make array the right length
+			extraSpots = extraSpots * (len(lineSplit) -1)
 
-		for j in range(0,len(lineSplit)-1,1):
-			extraSpots[j] = len(lineSplit[j]) + extraSpots[j-1]
-			if extraSpots[j] == extraSpots[j-1]:
-				extraSpots[j] = extraSpots[j] +1
+			for j in range(0,len(extraSpots),1):
+				if j == 0:
+					extraSpots[j] = len(lineSplit[j])
+				elif extraSpots[j] == extraSpots[j-1]:
+					extraSpots[j] = extraSpots[j-1] +1
+				else:
+					extraSpots[j] = len(lineSplit[j]) + extraSpots[j-1]
 
-				#make reference sequence full of Ns
-		if len(sequence) == 1:
-			if hlength-qlength >= 0:
-				sequence = sequence * (hlength + len(extraSpots))
-			else:
-				sequence = sequence * (qlength + len(extraSpots))
-				
+
+					#make reference sequence full of Ns
+			if len(sequence) == 1:
+				if hlength-qlength >= 0:
+					sequence = sequence * (hlength + len(extraSpots))
+				else:
+					sequence = sequence * (qlength + len(extraSpots))
+		else :
+			pass 
 				
 	if "<Hsp_hseq>" in line :
-		#split hit into array
-		lineSplit = list(blastParser(line))
-		
-		position = 0 
-
-
-		if len(sequence) >= len(lineSplit):
-			for i in range(0, len(lineSplit) -1 , 1):
+		if hitCount == 1 :
+			#split hit into array
+			lineSplit = list(blastParser(line))
+			
+			position = 0 
+			for i in range(0, len(lineSplit), 1):
 				sequence[start+position] = lineSplit[i]
 				position += 1
 
-		if len(sequence) < len(lineSplit):
-		#remove spots in the hit where the query was extended
-			for i in range(0, len(sequence) -1 , 1):
-				sequence[start+position] = lineSplit[i]
-				position += 1
 			for j in range(0,len(extraSpots),1):
-				del sequence[extraSpots[j]]
+				del sequence[int(extraSpots[j])]
+
+			if hlength-qlength >= 0:
+				while qlength < len(sequence):
+					del sequence[len(sequence)-1]
+
+		else :
+			pass
+					
+	if "</Hit>" in line :
+		if len(sequence) > 1:
+			file_name = outpath+'/'+locus_name+'.fasta'
+			file = open(file_name,'w') 
+			file.write(">roth\n")
+			#sys.stdout.write('\n')
+			for i in range(0, len(sequence), 1):
+				file.write(sequence[i])
+				#sys.stdout.write(sequence[i])
+				if ((i+1)%60 == 0 and i != 0):
+					#sys.stdout.write('\n')
+					file.write('\n') 
+			#sys.stdout.write('\n\n')
+			file.write('\n\n') 
+			file.close() 
 
 
-
-	if "</Hit>" in line :	
-		file_name = outpath+'/'+locus_name+'.fasta'
-		file = open(file_name,'w') 
-		file.write(">roth\n")
-		for i in range(0, len(sequence), 1):
-			file.write(sequence[i])
-			if (i % 60) == 0 & i != 0:
-				file.write('\n') 
-		file.write('\n') 
-		file.close() 
-
-#print as cells, with frames of sixty
 
 
 
