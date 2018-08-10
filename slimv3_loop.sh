2d5c8aa62b45816f69c67c93f6358f9ce2d53713
Felix Beaudry <felixbeaudry@esc2042-imac-hollister-wright.eeb.utoronto.ca>
2018-08-10 09:20:36 -0400

    polym

--- slimv3_loop.sh
+++ slimv3_loop.sh
@@ -5,9 +5,12 @@ do
 		for pro in {0..10}
 		do
 			python /ohta/felix.beaudry/scripts/reads2poly/slim_param_v3.py -s 0.${sel} -p 0.${pro} -t ${typ} >${typ}_${sel}_${pro}.slim 
+			echo "mean se slope" >${typ}_${sel}_${pro}.txt
 			for sim in {0..100}
 			do
-			/ohta/apps/SLiM/bin/slim ${typ}_${sel}_${pro}.slim | awk '$1 ~ "ratio"{print $2}' | perl /ohta/felix.beaudry/scripts/reads2poly/slim_stat.pl >>${typ}_${sel}_${pro}.txt
+			/ohta/apps/SLiM/bin/slim ${typ}_${sel}_${pro}.slim | awk '$1 ~ "ratio"{print $2}' >${typ}_${sel}_${pro}_${sim}.txt 
+			perl /ohta/felix.beaudry/scripts/reads2poly/slim_stat.pl ${typ}_${sel}_${pro}_${sim}.txt >>${typ}_${sel}_${pro}.txt
+			rm ${typ}_${sel}_${pro}_${sim}.txt
 			done
 		done
 	done
