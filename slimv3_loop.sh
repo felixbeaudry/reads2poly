02df56b5988257ee4c7393f5b4149d4f7076e6f8
Felix Beaudry <felixbeaudry@esc2042-imac-hollister-wright.eeb.utoronto.ca>
2018-08-10 10:09:20 -0400

    polym

--- slimv3_loop.sh
+++ slimv3_loop.sh
@@ -10,7 +10,7 @@ do
 			do
 			echo "${typ} ${sel} ${pro} ${sim}"
 			/ohta/apps/SLiM/bin/slim ${typ}_${sel}_${pro}.slim | awk '$1 ~ "ratio"{print $2}' >${typ}_${sel}_${pro}_${sim}.txt 
-			python /ohta/felix.beaudry/scripts/reads2poly/slim_stat.py ${typ}_${sel}_${pro}_${sim}.txt >>${typ}_${sel}_${pro}.txt
+			python /ohta/felix.beaudry/scripts/reads2poly/slim_stat.py -i ${typ}_${sel}_${pro}_${sim}.txt >>${typ}_${sel}_${pro}.txt
 			#rm ${typ}_${sel}_${pro}_${sim}.txt
 			done
 		done
