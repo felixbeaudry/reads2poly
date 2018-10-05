a54d69aec7d43b3ba9de66b44a8b2018e50b812b
Felix Beaudry <felixbeaudry@esc2042-imac-hollister-wright.eeb.utoronto.ca>
2018-10-05 15:35:08 -0400

    polym

--- sqlMerge.sh
+++ sqlMerge.sh
@@ -1,14 +1,21 @@
 
+#usage bash -i sqlMerge.sh rna.inds RhastatulusBase RhastatulusExp
+
 $1 = inds
 $2 = base
 $3 = out
 
+
+#cat A.list H.list N.list X.list >allLoci.list
+echo "create table ${base} (Loci VARCHAR(150));" | mysql -u felix.beaudry -D felix_rhastatulus
+python /ohta/felix.beaudry/scripts/reads2poly/intosql_base.py -i allLoci.list -c Loci | mysql -u felix.beaudry -D ${out}
+
+
 awk 'split($1,a,"."){print a[1] "" a[2]}' ${inds} >rna.sql.list
 while read ind
 do
-echo "create table ${ind} (Locus VARCHAR(150), mapped INT);" | mysql -u felix.beaudry -D ${out}
-#is file name now intosql.py?
-python /ohta/felix.beaudry/scripts/reads2poly/intosql.py -i ${ind}.reads.txt -c ${ind} | mysql -u felix.beaudry -D ${out}
+echo "create table ${ind} (Locus VARCHAR(150), mapped INT);" | mysql -u felix.beaudry -D felix_rhastatulus
+python /ohta/felix.beaudry/scripts/reads2poly/intosql.py -i ${ind}.reads.txt -c ${ind} | mysql -u felix.beaudry -D felix_rhastatulus
 done < rna.sql.list
 
 sqlMerge.py -i rna.sql.list -l ${base} -t ${out} >sqlMerging.sh
@@ -18,4 +25,8 @@ echo "select * from ${out};" | mysql ${out} >${out}.tsv
 
 
 #sql codes
-#CREATE DATABASE rhastatulus;
+#mysql
+#CREATE DATABASE felix_rhastatulus;
+#USE felix_rhastatulus;
+
+#cat A.list H.list N.list X.list >allLoci.list
